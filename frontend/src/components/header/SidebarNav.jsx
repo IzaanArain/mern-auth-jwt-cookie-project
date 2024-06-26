bc285afc993180409bc6b425b7b44daad4322522
IzaanArain <izaansaquib@gmail.com>
2024-06-26 08:06:32 +0500

    mern-auth

--- frontend/src/components/header/SidebarNav.jsx
+++ frontend/src/components/header/SidebarNav.jsx
@@ -1,4 +1,5 @@
 import './header.css'
+import { useState } from 'react';
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
@@ -9,22 +10,23 @@ const SidebarNav = () => {
     <>
       <header>
         <Navbar
-          expand={false}
+          expand={'lg'}
           variant="dark"
           bg="dark"
           data-bs-theme="dark"
-          className="sidebar-nav"
+          // className="custom-navbar"
         >
           <Container>
             <Navbar.Brand href="/" className="d-flex align-items-center">
               <span className="me-1">MERN-AUTH</span>
               <LiaCookieBiteSolid />
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="offcanvasNavbar" />
+            <Navbar.Toggle aria-controls="offcanvasNavbar"/>
             <Navbar.Offcanvas
               id="offcanvasNavbar"
               aria-labelledby="offcanvasNavbar"
               placement="start"
+              className="custom-offcanvas"
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title>
@@ -35,7 +37,7 @@ const SidebarNav = () => {
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
-                <Nav>
+                <Nav className='ms-auto'>
                   <Nav.Link href="/login">
                     <VscSignIn /> <span>Sign In</span>
                   </Nav.Link>
