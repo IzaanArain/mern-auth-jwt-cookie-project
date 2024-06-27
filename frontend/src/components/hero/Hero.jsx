5ca330c5e2586375671163eca0dbde0b33f23a6e
Izzan Saqib <izzan.saqib@binate.pk>
2024-06-27 12:26:51 +0500

    backend done

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -6,19 +6,27 @@ const Hero = () => {
     <>
       <section className="hero-section container-fluid p-5">
         {/* <Container className="h-100" fluid> */}
-          <Card className="hero-card h-100 bg-dark text-white border rounded-5">
-            <Card.Body className="container d-flex justify-content-center align-items-center flex-column">
-              <Card.Title><span>Hello World!</span></Card.Title>
-              <Card.Text className="text-center">
-              This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
-              </Card.Text>
-              <hr style={{ border: '1px solid white', width: '100%'}} />
-              <Card.Text className="text-center">
-              It uses utility classes for typography and spacing to space content out within the larger container.
-              </Card.Text>
-              <button className="btn btn-primary">Learn more</button>
-            </Card.Body>
-          </Card>
+        <Card className="h-100 border rounded-5" style={{ backgroundColor: 'black',color:'white' }} >
+          {/* bg-dark text-white */}
+          <Card.Body className="container d-flex justify-content-center align-items-center flex-column">
+            <Card.Title>
+              <span>Hello World!</span>
+            </Card.Title>
+            <Card.Text className="text-center">
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </Card.Text>
+            <hr style={{ border: "1px solid white", width: "100%" }} />
+            <Card.Text className="text-center">
+              It uses utility classes for typography and spacing to space
+              content out within the larger container.
+            </Card.Text>
+            <div className="d-flex flex-column flex-lg-row justify-content-lg-center gap-4">
+              <button className="btn btn-outline-info btn-lg ">Sign In</button>
+              <button className="btn btn-outline-info btn-lg">Register</button>
+            </div>
+          </Card.Body>
+        </Card>
         {/* </Container> */}
       </section>
     </>
