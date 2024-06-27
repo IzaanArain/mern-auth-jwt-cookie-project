7e82e0128bc67122a7a7a061c8ae75266efecb0a
Izzan Saqib <izzan.saqib@binate.pk>
2024-06-27 15:08:59 +0500

    backend done

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Form, Button, Row, Col,Card } from "react-bootstrap";
+import { Form, Button, Row, Col, Card } from "react-bootstrap";
 import FormContainer from "../../components/form/FormContainer";
-import "./auth.css"
+import "./auth.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,41 +15,41 @@ const Login = () => {
     <>
       <div>
         <FormContainer>
-         <Card className="auth-card card p-5">
-         <h1>Sign In</h1>
-          <Form onSubmit={submitHandler}>
-            <Form.Group className="my-2" controlId="email">
-              <Form.Label>Email Address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
+          <Card className="auth-card card p-5">
+            <h1>Sign In</h1>
+            <Form onSubmit={submitHandler}>
+              <Form.Group className="my-2" controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
 
-            <Form.Group className="my-2" controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
+              <Form.Group className="my-2" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
 
-            <Button type="submit" variant="primary" className="mt-3">
-              Sign In
-            </Button>
-            <Row className="py-3">
-              <Col>
-                <span>
-                  New Customer? <Link to="/register">Register</Link>
-                </span>
-              </Col>
-            </Row>
-          </Form>
-         </Card>
+              <Button type="submit" variant="primary" className="mt-3">
+                Sign In
+              </Button>
+              <Row className="py-3">
+                <Col>
+                  <span>
+                    New Customer? <Link to="/register">Register</Link>
+                  </span>
+                </Col>
+              </Row>
+            </Form>
+          </Card>
         </FormContainer>
       </div>
     </>
--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button, Row, Col, Card } from "react-bootstrap";
 import FormContainer from "../../components/form/FormContainer";
-import "./auth.css"
+import "./auth.css";
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,40 +15,40 @@ const Register = () => {
     <>
       <div>
         <FormContainer>
-        <Card className="auth-card card p-5">
-          <h1>Register</h1>
-          <Form onSubmit={submitHandler}>
-            <Form.Group className="my-2" controlId="email">
-              <Form.Label>Email Address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
+          <Card className="auth-card card p-5">
+            <h1>Register</h1>
+            <Form onSubmit={submitHandler}>
+              <Form.Group className="my-2" controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
 
-            <Form.Group className="my-2" controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
+              <Form.Group className="my-2" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
 
-            <Button type="submit" variant="primary" className="mt-3">
-              Sign In
-            </Button>
-            <Row className="py-3">
-              <Col>
-                <span>
-                  New Customer? <Link to="/register">Register</Link>
-                </span>
-              </Col>
-            </Row>
-          </Form>
+              <Button type="submit" variant="primary" className="mt-3">
+                Sign In
+              </Button>
+              <Row className="py-3">
+                <Col>
+                  <span>
+                    New Customer? <Link to="/register">Register</Link>
+                  </span>
+                </Col>
+              </Row>
+            </Form>
           </Card>
         </FormContainer>
       </div>
