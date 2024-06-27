747646542563a4e75025d0fca64cbcf378f6bf5b
Izzan Saqib <izzan.saqib@binate.pk>
2024-06-27 13:14:23 +0500

    backend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./components/header/Header"
 import HeaderOffcanvas from "./components/header/HeaderOffcanvas"
 import Home from "./pages/home/Home"
-
+import { Outlet } from "react-router-dom"
+import { Container } from "react-bootstrap"
 const App = () => {
   return (
     <>
     <Header/>
     {/* <HeaderOffcanvas/> */}
-    <Home/>
+   <Outlet/>
     </>
   )
 }
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { VscSignOut, VscSignIn } from "react-icons/vsc";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
@@ -10,19 +11,25 @@ const Header = () => {
       <header className="header-section">
         <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
-            <Navbar.Brand href="/" className="d-flex align-items-center">
-              <span className="me-1">MERN-AUTH</span>
-              <LiaCookieBiteSolid />
-            </Navbar.Brand>
+            <LinkContainer to="/">
+              <Navbar.Brand className="d-flex align-items-center">
+                <span className="me-1">MERN-AUTH</span>
+                <LiaCookieBiteSolid />
+              </Navbar.Brand>
+            </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
-                <Nav.Link href="/login">
-                  <VscSignIn /> <span>Sign In</span>
-                </Nav.Link>
-                <Nav.Link href="/register">
-                  <VscSignOut /> <span>Sign Up</span>
-                </Nav.Link>
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <VscSignIn /> <span>Sign In</span>
+                  </Nav.Link>
+                </LinkContainer>
+                <LinkContainer>
+                  <Nav.Link>
+                    <VscSignOut /> <span>Sign Up</span>
+                  </Nav.Link>
+                </LinkContainer>
               </Nav>
             </Navbar.Collapse>
           </Container>
