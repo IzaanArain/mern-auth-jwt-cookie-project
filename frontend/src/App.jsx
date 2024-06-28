bcd07d5c72c9bdac7e30705b3a5e5ec369051601
IzaanArain <izaansaquib@gmail.com>
2024-06-28 10:14:22 +0500

    mern-auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./components/header/Header"
 import HeaderOffcanvas from "./components/header/HeaderOffcanvas"
-import Home from "./pages/home/Home"
 import { Outlet } from "react-router-dom"
 import { Container } from "react-bootstrap"
 const App = () => {
