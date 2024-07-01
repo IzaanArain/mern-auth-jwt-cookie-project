e5abea405ba80e7c33110d5bf0fa392fcbcadd62
Izzan Saqib <izzan.saqib@binate.pk>
2024-07-01 16:39:55 +0500

    mern auth cookie project done

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,25 +1,34 @@
-import jwt from 'jsonwebtoken';
-import expressAsyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
+import jwt from "jsonwebtoken";
+import expressAsyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
-const protect = expressAsyncHandler(async (req,res,next) => {
-    let token;
-    token = req.cookies.jwt || req?.headers?.authorization?.split(" ")[1];
+const protect = expressAsyncHandler(async (req, res, next) => {
+  let token;
+  // token = req.cookies.jwt || req?.headers?.authorization?.split(" ")[1];
+  //   console.log("cookie",req.cookies.jwt);
+  //   console.log("authorization",req?.headers?.authorization);
+  if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  } else if (
+    req?.headers?.authorization &&
+    req?.headers?.authorization.startsWith("Bearer")
+  ) {
+    token = req?.headers?.authorization?.split(" ")[1];
+  }
 
-    if(token) {
-        try {
-            const decoded = jwt.verify(token,process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.userId).select('-password');
-            next();
-        } catch (err){
-            res.status(401);
-            throw new Error("Not authorized, invalid token")
-        }
-    } else {
-        res.status(401);
-        throw new Error("Not authorized, no token")
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
+      next();
+    } catch (err) {
+      res.status(401);
+      throw new Error("Not authorized, invalid token");
     }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
 });
 
-export {protect};
-
+export { protect };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,49 +1,57 @@
 import mongoose from "mongoose";
-const {Schema} = mongoose;
-import bcrypt from 'bcryptjs';
+const { Schema } = mongoose;
+import bcrypt from "bcryptjs";
 
-const userSchema = new Schema({
-    name:{
-        type:String,
-        required: true,
-        default: null
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      default: null,
     },
-    email:{
-        type:String,
-        required: true,
-        default: null,
-        unique:true,
-        lowercase: true,
-        trim:true
+    email: {
+      type: String,
+      required: true,
+      default: null,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    password:{
-        type:String,
-        required: true,
-        default: null
+    password: {
+      type: String,
+      required: true,
+      default: null,
     },
-    profileImage:{
-        type:String,
-        default: null
-    }
-},{timestamps:true});
+    profileImage: {
+      type: String,
+      default: null,
+    },
+    role: {
+      type: String,
+      enum: ["user"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
 
-userSchema.pre('save',async function (next) {
-    // if password field has not been modified then move on
-    if(!this.isModified("password")) {
-        next();
-    }
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password,salt);
-        next();
-    } catch (error) {
-        next(error)
-    }
+userSchema.pre("save", async function (next) {
+  // if password field has not been modified then move on
+  if (!this.isModified("password")) {
+    next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword,this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model("user",userSchema);
+const User = mongoose.model("user", userSchema);
 export default User;
